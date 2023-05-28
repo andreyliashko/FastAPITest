b8463221b3c67f6623253473f04e60d124cd70ac
alias <aliashko@ukr.net>
2023-05-28 17:35:58 +0300

    add some get, post functions

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from scremas import Book
+from typing import List
 
 app = FastAPI()
 
 
 @app.get('/')
 def hello():
-    return "hello"
+    return {'key': 'hello'}
+
+
+@app.get('/{pk}')
+def get_item(pk: int, q: str = None):
+    return {'key': pk, 'q': q}
+
+
+@app.get('/user/{user_id}/items/{item}/')
+def get_item(user_id: int, item: str):
+    return {'id': user_id, 'items': item}
+
+
+@app.post('/book/')
+def create_new_book(item: Book):
+    return item
+
+
+@app.get('/book/')
+def get_book(query: List[str] = Query(None, min_length=1, max_length=10, description='search book')):
+    return query
